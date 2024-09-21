0e8c3659f96c7c11108e4b8516f383229bfa4609
Mohd_Hassan_Ashraf <hassanashraf32553@gmail.com>
2024-09-21 19:15:44 +0530

    add new

--- src/app/property/page.jsx
+++ src/app/property/page.jsx
@@ -1,12 +1,50 @@
-import React from 'react'
+'use client'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function page() {
-  return (
-    <div className="w-full h-screen bg-green-400
-    ">
-      property
+
+  const [propertyList,setPropertylist] =  useState([])
+
+
+  const fetchdata = async () => {
+    try {
+      const res = await axios.get('http://localhost:5000/property/getall');
+      setPropertylist(res); // Set the fetched data into the state
+      console.log(res);
+      
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchdata()
+   },[])
+
+   return (
+    <div className="w-full h-screen   pt-20">
+      <div>
+        <h1>
+         {propertyList.map((item) => {
+          return <div key={item._id}>
+            <h1>{item.name}</h1>
+
+            hassan
+          </div>
+         })}
+        </h1>
+      </div>
     </div>
-  )
-}
+  );
+ 
+
+  }
+
+
+
+  
+
+
 
 export default page
