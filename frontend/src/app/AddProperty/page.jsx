8f7e4b5d9e3651bd16e7a27f3bb053ee3888405d
Mohd_Hassan_Ashraf <hassanashraf32553@gmail.com>
2024-09-21 23:46:20 +0530

    add new feature

--- frontend/src/app/AddProperty/page.jsx
+++ frontend/src/app/AddProperty/page.jsx
@@ -1,11 +1,77 @@
+'use client';
 import React from 'react'
+import axios, { Axios } from 'axios';
+import { useFormik } from 'formik'
+
+
+// form handling 
+
+// const propertyForm = useFormik({
+//   initialValues:{
+//     owner:"",
+//     location:"",
+//     country:"",
+//     city:"",
+//     price:"", 
+//     image:"lol",
+//   },
+//   onSubmit:(values,{resetForm,setSubmitting}) => {
+
+//    axios.post('http://localhost:5000/property/add',values)
+//       .then((response) => {
+//          console.log(response.status); 
+//          resetForm()  
+//       }).catch((err) => {
+//           console.log(err);
+          
+//           console.log(err.response?.data);
+//           setSubmitting(false)
+          
+//       });
+
+//   },
+// })
+
+
+
+
+// cloudnary 
+
+const handleUpload = (e) => {
+  let file = e.target.files[0]
+  const formData = new FormData()
+  formData.append('file',file)
+  formData.append('upload_preset','real estate project')
+  formData.append('cloud_name','dgiyyhb9f')
+ 
+ axios.post("https://api.cloudinary.com/v1_1/dgiyyhb9f/image/upload",formData,{
+   headers:{
+     'Content-Type':"multipart/form-data"
+   }
+ })
+ .then((result) => {
+ console.log(result.data);
+ toast.success('file upload successfully')
+ 
+ }).catch((err) => {
+   console.log(err);
+   toast.error("Failed to upload file")
+ 
+ })
+ 
+ 
+ }
+
+
 
 const page = () => {
   return (
     <div className='w-full h-screen bg-slate-300  pt-28 flex justify-center pb-[100px]'>
 
 
-<form action="" className='flex flex-col w-fit border-4 border-rose-600 p-10 h-fit rounded-xl gap-3   hover:shadow-red-500   bg-white hover:shadow-xl'>
+<form 
+// onSubmit={propertyForm.handleSubmit} 
+action="" className='flex flex-col w-fit border-4 border-rose-600 p-10 h-fit rounded-xl gap-3   hover:shadow-red-500   bg-white hover:shadow-xl'>
 
   
     <h1 className='font-bold text-xl text-rose-500'>Register Your Property</h1>
@@ -13,8 +79,11 @@ const page = () => {
 
   <div className="flex flex-col">
   <label htmlFor="owner" 
-  className="font-medium">Enter the Name of property owner:</label>
-  <input type="text" className="rounded-[5px] pl-3 border-slate-500 border-[1px] "/> 
+  className="font-medium">Enter property owner's name:</label>
+  <input 
+  // onChange={propertyForm.handleChange}
+  // value={propertyForm.values.owner}
+  type="text" className="rounded-[5px] pl-3 border-slate-500 border-[1px] "/> 
     </div>
 
   
@@ -23,7 +92,10 @@ const page = () => {
  <label htmlFor="location"
  className="font-medium"
  >Enter your location :</label>
- <input type="text" className="rounded-[5px] pl-3 border-slate-500 border-[1px] "/> 
+ <input
+//  onChange={propertyForm.handleChange}
+//  value={propertyForm.values.location}
+ type="text" className="rounded-[5px] pl-3 border-slate-500 border-[1px] "/> 
  </div>
 
 
@@ -31,7 +103,10 @@ const page = () => {
 <label htmlFor="country"
 className="font-medium"
 >Enter your Country :</label>
-<input type="text" className="rounded-[5px] pl-3 border-slate-500 border-[1px] "/> 
+<input
+// onChange={propertyForm.handleChange}
+// value={propertyForm.values.country}
+type="text" className="rounded-[5px] pl-3 border-slate-500 border-[1px] "/> 
 </div>
 
 
@@ -39,7 +114,10 @@ className="font-medium"
 <label htmlFor="city"
 className="font-medium"
 >Enter your City :</label>
-<input type="text" className="rounded-[5px] pl-3 border-slate-500 border-[1px] "/> 
+<input
+// onChange={propertyForm.handleChange}
+// value={propertyForm.values.city}
+type="text" className="rounded-[5px] pl-3 border-slate-500 border-[1px] "/> 
 </div>
 
 
@@ -47,20 +125,27 @@ className="font-medium"
 <label htmlFor="price" 
 className="font-medium"
 >Enter Price :</label>
-<input type="text" className="rounded-[5px] pl-3 border-slate-500 border-[1px] "/> 
+<input
+// onChange={propertyForm.handleChange}
+// value={propertyForm.values.email}
+type="text" className="rounded-[5px] pl-3 border-slate-500 border-[1px] "/> 
   </div> 
 
  <div className="flex flex-col">
  <label htmlFor="image"
  className="font-medium"
  >Upload Image :</label>
- <input type="file" className="rounded-[5px]  border-slate-500 border-[1px] "/> 
+ <input 
+ onChange={handleUpload}
+ type="file" className="rounded-[5px]  border-slate-500 border-[1px] "/> 
  </div>
 
 
 
 
- <input type="submit" className="rounded-[5px]   bg-rose-500 text-white font-semibold p-1 mt-2"/> 
+ <input 
+
+ type="submit" className="rounded-[5px]   bg-rose-500 text-white font-semibold p-1 mt-2"/> 
 
 
 
--- frontend/src/app/property/page.jsx
+++ frontend/src/app/property/page.jsx
@@ -1,5 +1,6 @@
 'use client'
 import axios from 'axios'
+import Image from 'next/image';
 import React, { useEffect, useState } from 'react'
 
 function page() {
@@ -10,8 +11,8 @@ function page() {
   const fetchdata = async () => {
     try {
       const res = await axios.get('http://localhost:5000/property/getall');
-      setPropertylist(res); // Set the fetched data into the state
-      console.log(res);
+      setPropertylist(res.data); // Set the fetched data into the state
+      console.log(res.data);
       
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -22,19 +23,60 @@ function page() {
     fetchdata()
    },[])
 
-   return (
-    <div className="w-full h-screen   pt-20">
-      <div>
-        <h1>
-         {propertyList.map((item) => {
-          return <div key={item._id}>
-            <h1>{item.name}</h1>
 
-            hassan
-          </div>
-         })}
-        </h1>
-      </div>
+
+   function deleteuser(id){
+     axios.delete("http://localhost:5000/property/delete/"+id)
+     .then((result) =>{
+       fetchdata()
+     })
+     .catch((err) => {
+       console.log(err);
+      
+     })
+ }
+
+
+
+
+   return (
+    <div className='pt-48 w-full h-screen px-20'>
+
+<table className="border-2 border-slate-300 rounded-xl">
+  <thead className='border-b-2 text-left bg-blue-500 text-white rounded-xl'>
+    <tr className='rounded-xl'>
+    <th className='p-3 text-lg sm:text-sm'>Property owner Id</th>
+      <th className='p-3 text-lg sm:text-sm'>Owner Name</th>
+      <th className='p-3  sm:text-base'>Location</th>
+      <th className='p-3 text-lg sm:text-base'>City</th>
+      <th className='p-3 text-lg sm:text-base'>Country</th>
+      <th className='p-3 text-lg sm:text-base'>Price</th>
+      <th className='p-3 text-lg sm:text-base'>Delete</th>
+
+
+    </tr>
+  </thead>
+  <tbody>
+    {propertyList.map((property) => {
+      return<tr key={property._id}>
+        <td className='p-3'>{property._id}</td>
+<td className='p-3'>{property.owner}</td>
+<td className='p-3'>{property.location}</td>
+<td className='p-3'>{property.city}</td>
+<td className='p-3'>{property.country}</td>
+<td className='p-3'>{property.price}</td>
+
+<td>
+  <button className="mr-4 px-3 py-1 bg-red-500 rounded-full text-white"
+  onClick={() => {deleteuser(property._id)}}
+  >Delete</button>
+</td>
+      </tr>
+    })}
+  </tbody>
+</table>
+
+
     </div>
   );
  
